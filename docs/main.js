50e3a4af3dae1d88d376d99be194ba90ee4cc768
Ben Scarletti <scarletti.ben@gmail.com>
2025-07-24 11:51:26 +0100

    added URL search parameter functionality for demos

--- docs/main.js
+++ docs/main.js
@@ -24,17 +24,45 @@ const links = /** @type {HTMLDivElement} */
  */
 (() => {
 
-    // ~ Add links for all of the demos
-    for (const [name, demo] of Object.entries(demos)) {
-        const link = document.createElement('a');
-        link.href = '#';
-        link.textContent = name;
-        link.onclick = (event) => {
-            event.preventDefault();
-            links.remove();
-            demo(main);
-        };
-        links.appendChild(link);
+    // ~ Handle back button to reset
+    window.addEventListener('popstate', () => {
+        location.reload();
+    });
+
+    // ~ Check for URL search parameter ?demo=nane
+    const params = new URLSearchParams(location.search);
+    let demoName;
+    if (params.has('demo')) {
+        demoName = params.get('demo');
+    }
+
+    if (demoName) {
+
+        // ~ Start the demo given in search parameters
+        for (const [name, demo] of Object.entries(demos)) {
+            if (demoName === name) {
+                links.remove();
+                demo(main);
+                break;
+            }
+        }
+        
+    } else {
+
+        // ~ Add links for all of the demos
+        for (const [name, demo] of Object.entries(demos)) {
+            const link = document.createElement('a');
+            link.href = '#';
+            link.textContent = name;
+            link.onclick = (event) => {
+                event.preventDefault();
+                history.pushState({}, '', location.href);
+                links.remove();
+                demo(main);
+            };
+            links.appendChild(link);
+        }
+
     }
 
 })();
